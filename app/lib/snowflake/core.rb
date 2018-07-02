e47b2e5a256e594d3347dcf750505718fc0d47c4
parasquid <tristan.gomez@gmail.com>
2018-07-02 05:22:58 +0300

    convert milestones to url parameters
    the original medium snowflake calls these milestones, so we're using the
    same terminology here. the idea being that we can just call to_params on
    the instantiated object and we can get a string that we can then just
    append to the proper url

--- app/lib/snowflake/core.rb
+++ app/lib/snowflake/core.rb
@@ -1,6 +1,14 @@
+require "uri"
 module Snowflake
   class Core
-    def initialize
+    def initialize(name:, title:, milestones:)
+      @name = name
+      @title = title
+      @milestones = milestones
+    end
+
+    def to_params
+      (@milestones.values + [URI.escape(@name)] + [URI.escape(@title)]).join(",")
     end
   end
 end
--- spec/lib/snowflake/core_spec.rb
+++ spec/lib/snowflake/core_spec.rb
@@ -3,8 +3,30 @@ require "lib/snowflake/core.rb"
 
 describe Snowflake::Core do
   Given(:klass) { Snowflake::Core }
-  context "it can be initialized" do
-    When(:instance) { klass.new }
-    Then { instance != nil }
+  context "it transforms milestones into a url" do
+    Given(:name) { "Cersei Lannister" }
+    Given(:title) { "Staff Engineer" }
+    Given(:milestones) {
+      {
+        mobile: 1,
+        web_client: 2,
+        foundations: 3,
+        servers: 2,
+        project_management: 4,
+        communication: 1,
+        craft: 1,
+        initialitive: 4,
+        career_development: 3,
+        org_design: 2,
+        wellbeing: 0,
+        accomplishment: 4,
+        mentorship: 2,
+        evangelism: 2,
+        recruiting: 3,
+        community: 0,
+      }
+    }
+    When(:instance) { klass.new(name: name, title: title, milestones: milestones) }
+    Then { instance.to_params == "1,2,3,2,4,1,1,4,3,2,0,4,2,2,3,0,Cersei%20Lannister,Staff%20Engineer" }
   end
 end
